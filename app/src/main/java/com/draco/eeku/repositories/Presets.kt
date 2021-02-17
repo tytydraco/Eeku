1d9981464166bcd9629c2470be78d36a7ac876e6
tytydraco <tylernij@gmail.com>
2021-02-16 17:28:44 -0800

    Migrate freq gain maps to presets
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/eeku/repositories/Presets.kt
+++ app/src/main/java/com/draco/eeku/repositories/Presets.kt
@@ -6,11 +6,27 @@ val Presets = listOf(
     Preset(
         "mask",
         "Flat",
-        FrequencyGainMaps.FLAT
+        mapOf(
+            Frequencies.SUB_BASS to         0.00f,
+            Frequencies.BASS to             0.00f,
+            Frequencies.LOW_MID to          0.00f,
+            Frequencies.MID to              0.00f,
+            Frequencies.UPPER_MID to        0.00f,
+            Frequencies.PRESENCE to         0.00f,
+            Frequencies.BRILLIANCE to       0.00f
+        )
     ),
     Preset(
         "v_shape",
         "V Shaped",
-        FrequencyGainMaps.V_SHAPE
+        mapOf(
+            Frequencies.SUB_BASS to         10.00f,
+            Frequencies.BASS to             5.00f,
+            Frequencies.LOW_MID to          -5.00f,
+            Frequencies.MID to              0.00f,
+            Frequencies.UPPER_MID to        0.00f,
+            Frequencies.PRESENCE to         5.00f,
+            Frequencies.BRILLIANCE to       10.00f
+        )
     )
 )
