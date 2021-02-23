ee63e8321dbdbad143ffdda76a3dbaaa87beac12
tytydraco <tylernij@gmail.com>
2021-02-23 15:53:56 -0800

    Part 3 of recycler migration
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/eeku/utils/PresetChartModelFactory.kt
+++ app/src/main/java/com/draco/eeku/utils/PresetChartModelFactory.kt
@@ -4,22 +4,25 @@ import android.graphics.Color
 import com.draco.eeku.models.Preset
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartZoomType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.github.aachartmodel.aainfographics.aaoptionsmodel.AADataLabels
 
 class PresetChartModelFactory(private val preset: Preset) {
     private val chartModel = AAChartModel()
-        .chartType(AAChartType.Areaspline)
+        .chartType(AAChartType.Spline)
         .dataLabelsEnabled(true)
         .series(getSeries())
         .categories(getCategories())
         .yAxisVisible(false)
         .xAxisVisible(false)
-        .backgroundColor("#00000000")
+        .backgroundColor(Color.TRANSPARENT)
         .yAxisGridLineWidth(0f)
+        .tooltipEnabled(false)
 
     private fun getSeries() = arrayOf(
         AASeriesElement()
+            .name("Gain")
             .showInLegend(false)
             .dataLabels(AADataLabels().enabled(false))
             .data(preset.map.values.toTypedArray())
